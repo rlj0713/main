eb875a221528d01e08cfc44bfe2fa3f5b6c32d7f
Reid Jackson <72239107+rlj0713@users.noreply.github.com>
2020-10-17 21:42:50 -0400

    Create Function to Print Fibonacci Sequence

--- js-basics/evenfibonacci.js
+++ js-basics/evenfibonacci.js
@@ -1,4 +1,10 @@
 
-let x = 'seriously'
+for (let i = 0; i < 40; i++) {
+    console.log(fibonacci(i));
+}
 
-console.log(x);
+function fibonacci(i) {
+    let a = ((1 + Math.sqrt(5)) / 2);
+    let b = ((1 - Math.sqrt(5)) / 2);
+    return Math.round(1 / Math.sqrt(5) * ((a ** i) - (b ** i)))
+}
