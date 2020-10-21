eb211550afb27a9b7dae04764c7250b0b3b38884
Reid Jackson <72239107+rlj0713@users.noreply.github.com>
2020-10-20 21:05:24 -0400

    Create function to print primes

---
+++ js-basics/primeNumber.js
@@ -0,0 +1,20 @@
+
+// Get Composite Numbers
+function isComposite(a) {
+    for (let i = (a - 1); i > 1; i--) {
+        if (a % i == 0) {
+            return true
+        }
+    }
+}
+
+// Print all Primes between 2 limits
+function printPrimes(low, high) {
+    for (let i = low; i < high; i++) {
+        if (isComposite(i) !== true)
+            console.log(i);
+    }
+}
+
+printPrimes(2, 10);
+
